3c8449c42578effe52dee5898b86ff07dc30edf8
Enrique <enrique.hernandez@jala.university>
2025-01-24 17:41:30 -0600

    Update map

--- Game/Scripts/Characters/Ships/ship_01.gd
+++ Game/Scripts/Characters/Ships/ship_01.gd
@@ -1,8 +1,13 @@
 @icon("res://Assets/Sprites/Ships/body_01.png")
 class_name Ship_01
 extends ShipGeneric
-
+@onready var camera :Camera2D= $Camera2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 		
+
+func _process(delta: float) -> void:
+	#region look front
+	camera.rotation = rotation
+	#endregion
